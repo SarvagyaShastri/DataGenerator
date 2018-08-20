5598360818c0e2d0e25bc18db793582522469fb8
Sarvagya Shastri <sarvagyas12@gmail.com>
2018-08-20 00:11:34 -0700

    Coin change one solution

--- CoinChangeOne.java
+++ CoinChangeOne.java
@@ -1,4 +1,22 @@
-package PACKAGE_NAME;
-
 public class CoinChangeOne {
+
+
+    public static void main(String args[]) {
+        DataGeneratorUtil dataGeneratorUtil = new DataGeneratorUtil(50, true);
+        int N = dataGeneratorUtil.randomInteger(5, 15);
+        int[] coins = dataGeneratorUtil.generateIntegerArrayWithUniqueElements(4, 1, N);
+        System.out.println(findWays(N, coins));
+    }
+
+    private static int findWays(int n, int[] coins) {
+        int dp[] = new int[n + 1];
+        dp[0] = 1;
+        for (int coin: coins
+             ) {
+            for (int i = coin; i <= n; i++) {
+                dp[i] += dp[i - coin];
+            }
+        }
+        return dp[n];
+    }
 }
