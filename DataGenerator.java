bb2c210f436d9a32fc474aee1f4fb986f607397e
Sarvagya Shastri <sarvagyas12@gmail.com>
2018-08-19 23:33:33 -0700

    Basic methods filled in, need testing

--- DataGenerator.java
+++ DataGenerator.java
@@ -1,37 +1,92 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Random;
+import java.util.Scanner;
+
+
+// Static iterator
+// Throw output on standard out
+
 
 class DataGeneratorUtil {
     private final int seed;
-
+    Random r;
+    static ArrayList<String> words;
+    String[] stringArray;
     public DataGeneratorUtil(int seed) {
         this.seed = seed;
+        this.r = new Random(seed);
+        words = new ArrayList<>();
+        readWords();
+    }
+
+    private void readWords() {
+        File f = new File("/Users/sarvagyas/Documents/GeeksDP/src/words.txt");
+        Scanner sc = null;
+        try {
+            sc = new Scanner(f);
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found. Please make sure words.txt is in the same path as the source file.");
+            return;
+        }
+        sc.useDelimiter("\n");
+        while (sc.hasNext()) {
+            words.add(sc.next());
+        }
+    }
+
+
+    public int randomInteger(int minValue, int maxValue) {
+        return r.nextInt(maxValue - minValue + 1) + minValue;
     }
 
     public int[] generateIntegerArray(int size, int minValue, int maxValue) {
         int array[] = new int[size];
-        Random r = new Random(seed);
         for (int i = 0; i < size; i++) {
             array[i] = r.nextInt(maxValue - minValue + 1) + minValue;
         }
         return array;
     }
 
-    public void generateIntegerArrayWithVariableRowSizes(int minRowLength, int maxRowLength, int minValue, int maxValue) {
-
+    public int[] generateIntegerArrayWithUniqueElements(int size, int minValue, int maxValue) {
+        HashSet<Integer> hashSet = new HashSet<>();
+        int array[] = new int[size];
+        while (hashSet.size() < size) {
+            hashSet.add(r.nextInt(maxValue - minValue + 1) + minValue);
+        }
+        int index = 0;
+        for (int i: hashSet
+             ) {
+            if (index < size) array[index++] = i;
+        }
+        return array;
     }
 
-    public void generateStringArray(int size, int stringLength) {
-        String strArray[] = new String[size];
 
+    public String[] generateWordArray(int arraySize) {
+        stringArray = new String[arraySize];
+        for (int i = 0; i < arraySize; i++) {
+            int randomIndex = r.nextInt(words.size() - i) + i;
+            stringArray[i] = words.get(randomIndex);
+        }
+        return stringArray;
     }
 
-    public void generateSentences() {
-
+    public String generateWord() {
+        return words.get(r.nextInt(words.size()));
     }
 
-    public void generateStringArrayWithVariableLengthStrings(int minSize, int maxSize, int stringLength) {
-
+    public String generateString(int maxWords) {
+        String sentence = "";
+        for (int i = 0; i < maxWords; i++) {
+            sentence += words.get(r.nextInt(words.size())) + " ";
+        }
+        return sentence.trim();
     }
+
+
 
     public int[][] generateIntegerMatrix(int rows, int columns, int minValue, int maxValue) {
         int matrix[][] = new int[rows][columns];
@@ -45,13 +100,9 @@ class DataGeneratorUtil {
     }
 }
 
-public class DataGenerator {
+/*public class DataGenerator {
     public static void main(String[] args) {
         DataGeneratorUtil dataGeneratorUtil = new DataGeneratorUtil(5);
-        int[] ints = dataGeneratorUtil.generateIntegerArray(10, 0, 10);
-        for (int i: ints
-             ) {
-            System.out.println(i);
-        }
+        System.out.println(dataGeneratorUtil.generateString(4));
     }
-}
+}*/
